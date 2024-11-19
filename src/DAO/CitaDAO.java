f08e47bf530b251468ef09de5754d65e1ed7fcf9
andre <andre@Andrey>
2024-11-18 19:23:05 -0600

    Avance 46

--- src/DAO/CitaDAO.java
+++ src/DAO/CitaDAO.java
@@ -6,9 +6,12 @@ import BLL.Condicion;
 import BLL.Evaluacion;
 import BLL_Motivos.*;
 import BLL.Paciente;
+import BLL.Sexo;
 import BLL_PruebaLaboratorio.*;
 import DAO.ConeccionDB;
 import  java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CitaDAO {
     
@@ -27,13 +30,13 @@ public class CitaDAO {
         
         String sqlInsertarCita_Actitud = "INSERT INTO Cita_Actitud (id_cita, id_actitud) VALUES (?, ?)";
         
-        String sqlInsertarCita_Condicion = "INSERT INTO Cita_Cnodicion (id_cita, id_condicion) VALUES (?, ?)";
+        String sqlInsertarCita_Condicion = "INSERT INTO Cita_Condicion (id_cita, id_condicion) VALUES (?, ?)";
 
         try (Connection conexion = ConeccionDB.conectarBaseDatos()) {
-            conexion.setAutoCommit(false); // Iniciar la transacción
+            conexion.setAutoCommit(false);
 
             try (PreparedStatement pstmtCita = conexion.prepareStatement(sqlInsertCita, Statement.RETURN_GENERATED_KEYS)) {
-                // Insertar la cita
+
                 pstmtCita.setInt(1, paciente.getId_paciente());
                 pstmtCita.setString(2, cita.getDiagnostico());
                 pstmtCita.setString(3, cita.getIndicaciones());
@@ -45,12 +48,10 @@ public class CitaDAO {
 
                 pstmtCita.executeUpdate();
 
-                // Obtener el ID de la cita insertada
                 try (ResultSet generatedKeys = pstmtCita.getGeneratedKeys()) {
                     if (generatedKeys.next()) {
                         int id_cita = generatedKeys.getInt(1);
 
-                        // Insertar en la tabla Cita_Motivo
                         if (!cita.getArrayMotivo().isEmpty()) {
 
                             try (PreparedStatement pstmtCitaMotivo = conexion.prepareStatement(sqlInsertCitaMotivo)) {
@@ -63,10 +64,10 @@ public class CitaDAO {
                                     } else {
                                         pstmtCitaMotivo.setNull(3, java.sql.Types.INTEGER);
                                     }
-                                    pstmtCitaMotivo.addBatch(); // Agregar al batch
+                                    pstmtCitaMotivo.addBatch();
                                 }
 
-                                pstmtCitaMotivo.executeBatch(); // Ejecutar todos los inserts de una vez
+                                pstmtCitaMotivo.executeBatch();
                             }
                         }
                         if (!cita.getArrayPruebaLaboratorio().isEmpty()) {
@@ -100,7 +101,7 @@ public class CitaDAO {
                                         }
                                     }
                                 }
-                                pstmCita_Evaluacion.executeBatch(); // Ejecutar los inserts en Cita_Evaluacion
+                                pstmCita_Evaluacion.executeBatch();
                             }
                         }
                         try(PreparedStatement pstmCita_Actitud = conexion.prepareStatement(sqlInsertarCita_Actitud)){
@@ -123,7 +124,7 @@ public class CitaDAO {
                     }
                 }
                 conexion.commit();
-                System.out.println("La cita y los motivos fueron insertados exitosamente.");
+                System.out.println("La cita fue insertada exitosamente.");
             } catch (SQLException e) {
                 conexion.rollback();
                 throw e;
@@ -131,5 +132,66 @@ public class CitaDAO {
                 conexion.setAutoCommit(true);
             }
         }
+    }
+    
+    public List<Cita> consultarCitasPorPaciente(int id_paciente) throws SQLException {
+        List<Cita> arrayCitas = new ArrayList<>();
+        String sqlConsultarCita = "SELECT id_cita, diagnostico, indicaciones, fechaCita, frecuenciaCardiaca, frecuenciaRespiratoria, pulso, temperatura from Cita where id_Paciente = " + id_paciente;
+
+        try {
+            try (Connection conexion = ConeccionDB.conectarBaseDatos();
+                    PreparedStatement pstmConsultarCita = conexion.prepareStatement(sqlConsultarCita);
+                    ResultSet rsCitas = pstmConsultarCita.executeQuery()) {
+                
+                while (rsCitas.next()) {
+                    int id_cita = rsCitas.getInt("id_cita");
+                    String disgnostico = rsCitas.getString("diagnostico").trim();
+                    String indicaciones = rsCitas.getString("indicaciones").trim();
+                    Date fechaCita = rsCitas.getDate("fechaCita");
+                    int frecuenciaCardiaca = rsCitas.getInt("frecuenciaCardiaca");
+                    int frecuenciaRespiratoria = rsCitas.getInt("frecuenciaRespiratoria");
+                    int pulso = rsCitas.getInt("pulso");
+                    int temperatura = rsCitas.getInt("temperatura");
+
+                    Cita cita = new Cita(id_cita, disgnostico, indicaciones, fechaCita, frecuenciaCardiaca, frecuenciaRespiratoria, pulso, temperatura);
+
+                    List<Actitud> arrayActitudes = new ArrayList<>();
+                    
+                    String sqlConsultaActitudes = "SELECT a.id_actitud, a.descripcion "
+                            + "FROM Actitud a "
+                            + "INNER JOIN Cita_Actitud ca ON a.id_actitud = ca.id_actitud "
+                            + "WHERE ca.id_cita = " + id_cita;
+
+                    try (PreparedStatement pstmConsultarActitudes = conexion.prepareStatement(sqlConsultaActitudes);
+                            ResultSet rsActitudes = pstmConsultarActitudes.executeQuery()) {
+                        while (rsActitudes.next()) {
+                            Actitud actitud = Actitud.valueOf(rsActitudes.getString("descripcion").trim());
+                            arrayActitudes.add(actitud);
+                        }
+                        cita.setArrayActitud(arrayActitudes);
+                    }
+                    
+                    List<Condicion> arrayCondiciones = new ArrayList<>();
+                    
+                    String sqlConsultaCondiciones = "SELECT co.id_condicion, co.descripcion "
+                            + "FROM Condicion co "
+                            + "INNER JOIN Cita_Condicion cc ON co.id_condicion = cc.id_condicion "
+                            + "WHERE cc.id_cita = " + id_cita;
+                    
+                    try (PreparedStatement pstmConsultarActitudes = conexion.prepareStatement(sqlConsultaCondiciones);
+                            ResultSet rsCondiciones = pstmConsultarActitudes.executeQuery()) {
+                        while (rsCondiciones.next()) {
+                            Condicion condicion = Condicion.valueOf(rsCondiciones.getString("descripcion").trim());
+                            arrayCondiciones.add(condicion);
+                        }
+                        cita.setArrayCondicion(arrayCondiciones);
+                    }
+                    arrayCitas.add(cita);
+                }
+            }
+        } catch (SQLException e) {
+            throw e;
+        }
+        return arrayCitas;
     }
 }
